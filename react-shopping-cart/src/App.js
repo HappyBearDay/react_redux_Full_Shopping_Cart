0fd89899846d6e4fc5a99e47acc58c576e9c760d
happybearday <anishamroun2@gmail.com>
2020-08-06 01:47:00 +0200

    Part 05 : Product List Component

--- react-shopping-cart/src/App.js
+++ react-shopping-cart/src/App.js
@@ -1,17 +1,34 @@
 import React from 'react';
+import data from "./data.json"
+import Products from './components/Products';
 
-function App() {
+class App extends React.Component {
+
+  constructor(){
+    super();
+    this.state = {
+      products: data.products,
+      size: "",
+      sort: ""
+    }
+  }
+  
+  render() {
   return (
     <div className="grid-container">
       <header>
         <a href="/">React Shopping Cart</a>
       </header>
       <main>
-        Product List
+        <div className="content">
+          <div className="main">Product</div>
+          <Products products={this.state.products}/>
+          <div className="sidebar">Cart Items</div>
+        </div>
       </main>
       <footer>All right is reserved.</footer>
     </div>
-  );
+  ); }
 }
 
 export default App;
